27a6cb53aac719ae857871aebcc85f0b7efa32df
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-01-12 18:34:18 +0900

    프로젝트 초기 설정
    폴더 생성
    eslint prettier 설정
    폰트(나눔스퀘어 네오) 설정
    경로 alias
    craco styled-components react-router-dom 설치

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+    },
+  },
+};
