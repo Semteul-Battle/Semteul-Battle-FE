a2aecad8ac956255c227a2ac45f367468d47b443
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-01-13 23:21:44 +0900

    [chore] 대회 페이지 경로 조정
    이전 대회랑 예정/진행 대회로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
-import Battle from '@pages/Battle';
 import Announcement from '@pages/Announcement';
 import QA from '@pages/QA';
 import Introduction from '@pages/Introduction';
 import LogIn from '@pages/LogIn';
 import SignUp from '@pages/SignUp';
 import Header from '@layouts/Header';
+import BattleOn from '@pages/BattleOn';
+import BattlePrev from '@pages/BattlePrev';
 
 const App = () => {
   return (
@@ -15,7 +16,8 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/battle' element={<Battle />} />
+        <Route path='/battle/prev' element={<BattlePrev />} />
+        <Route path='/battle/on' element={<BattleOn />} />
         <Route path='/announcement' element={<Announcement />} />
         <Route path='/qa' element={<QA />} />
         <Route path='introduction' element={<Introduction />} />
--- src/pages/Battle/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Battle = () => {
-  return <div>대회 페이지</div>;
-};
-
-export default Battle;
