f3a1a4f0ee2714b9e32e8e7f51731539e723cad0
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-01-13 15:21:40 +0900

    [feat] 페이지 생성 및 경로 지정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
+import Battle from '@pages/Battle';
+import Announcement from '@pages/Announcement';
+import QA from '@pages/QA';
+import Introduction from '@pages/Introduction';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/battle' element={<Battle />} />
+      <Route path='/announcement' element={<Announcement />} />
+      <Route path='/qa' element={<QA />} />
+      <Route path='introduction' element={<Introduction />} />
+      <Route path='login' element={<LogIn />} />
+      <Route path='signup' element={<SignUp />} />
     </Routes>
   );
 };
---
+++ src/pages/QA/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const QA = () => {
+  return <div>질문 페이지</div>;
+};
+
+export default QA;
