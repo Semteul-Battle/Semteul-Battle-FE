4985ef0c918420f9d5b5d05c8115eee5d5e47c54
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-01-23 19:24:28 +0900

    [chore] 배틀 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
 import Announcement from '@pages/Announcement';
+import Battle from '@pages/Battle';
 import QA from '@pages/QA';
 import Introduction from '@pages/Introduction';
 import LogIn from '@pages/LogIn';
 import SignUp from '@pages/SignUp';
 import Header from '@layouts/Header';
-import BattleOn from '@pages/BattleOn';
-import BattlePrev from '@pages/BattlePrev';
 
 const App = () => {
   return (
@@ -16,8 +15,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/battle/prev' element={<BattlePrev />} />
-        <Route path='/battle/on' element={<BattleOn />} />
+        <Route path='/battle' element={<Battle />} />
         <Route path='/announcement' element={<Announcement />} />
         <Route path='/qa' element={<QA />} />
         <Route path='introduction' element={<Introduction />} />
---
+++ src/pages/Battle/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Battle = () => {
+  return <div>배틀 페이지</div>;
+};
+
+export default Battle;
