8b6bfe22483cf2e4471986b16ce0ab8fea9f9441
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-01-25 04:16:21 +0900

    [add] 대회 리스트 정렬 기능 구현

--- src/pages/Battle/index.jsx
+++ src/pages/Battle/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BattleButton,
   BattleDuration,
@@ -16,45 +16,65 @@ import BattleContent from '@pages/Battle/BattleContent';
 
 // erd 대회 테이블 state 삭제
 
-const Battles = [
-  {
-    id: 1,
-    name: '봄봄배 코드컵',
-    autority: 1,
-    start: new Date('2024-03-01T15:00'),
-    end: new Date('2024-03-01T18:00'),
-  },
-  {
-    id: 2,
-    name: '봄봄배 코드컵',
-    autority: 1,
-    start: new Date('2024-03-01T15:00'),
-    end: new Date('2024-03-01T18:00'),
-  },
-  {
-    id: 3,
-    name: '봄봄배 코드컵',
-    autority: 1,
-    start: new Date('2024-03-01T15:00'),
-    end: new Date('2024-03-01T18:00'),
-  },
-  {
-    id: 4,
-    name: '봄봄배 코드컵',
-    autority: 1,
-    start: new Date('2024-03-01T15:00'),
-    end: new Date('2024-03-01T18:00'),
-  },
-  {
-    id: 5,
-    name: '봄봄배 코드컵',
-    autority: 1,
-    start: new Date('2024-03-01T15:00'),
-    end: new Date('2024-03-01T18:00'),
-  },
-];
-
 const Battle = () => {
+  const [battles, setBattles] = useState([]);
+
+  useEffect(() => {
+    const rawBattles = [
+      {
+        id: 1,
+        name: '봄봄배 코드컵',
+        autority: 1,
+        start: new Date('2024-01-25T04:00'),
+        end: new Date('2024-01-25T06:00'),
+      },
+      {
+        id: 2,
+        name: '봄봄배 코드컵',
+        autority: 1,
+        start: new Date('2023-03-01T15:00'),
+        end: new Date('2023-03-01T18:00'),
+      },
+      {
+        id: 3,
+        name: '봄봄배 코드컵',
+        autority: 1,
+        start: new Date('2024-03-02T15:00'),
+        end: new Date('2024-03-02T18:00'),
+      },
+      {
+        id: 4,
+        name: '봄봄배 코드컵',
+        autority: 1,
+        start: new Date('2024-03-03T15:00'),
+        end: new Date('2024-03-03T18:00'),
+      },
+      {
+        id: 5,
+        name: '봄봄배 코드컵',
+        autority: 1,
+        start: new Date('2024-03-04T15:00'),
+        end: new Date('2024-03-04T18:00'),
+      },
+    ];
+
+    const sortBattles = [...rawBattles];
+    const currentTime = new Date();
+    sortBattles.sort((left, right) => {
+      const leftStatus =
+        left.end < currentTime ? 2 : left.start > currentTime ? 1 : 0;
+      const rightStatus =
+        right.end < currentTime ? 2 : right.start > currentTime ? 1 : 0;
+
+      if (leftStatus !== rightStatus) {
+        return leftStatus - rightStatus;
+      }
+      return right.start - left.start;
+    });
+
+    setBattles([...sortBattles]);
+  }, []);
+
   return (
     <FlexBox>
       <BattleWrapper>
@@ -81,7 +101,7 @@ const Battle = () => {
           </BattleInfoWrapper>
         </BattleListWrapper>
         <BattleList>
-          {Battles.map((battle) => (
+          {battles.map((battle) => (
             <BattleContent key={battle.id} battle={battle} />
           ))}
         </BattleList>
